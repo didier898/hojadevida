4e4a9f77c64eabd5ef46486a3ec3549d094d86fb
Didier <didier002010@gmail.com>
2023-07-06 10:17:10 -0500

    commit configuración inicial

---
+++ src/components/Heather.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Mi Hoja de Vida</h1>
+      <p>Ingeniero Electrónico</p>
+    </header>
+  );
+}
+
+export default Header;
